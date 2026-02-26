25c5ed7746cb2f0d3c1f8b5358a82a8949b8c3ab
Ole-Kristian Wigum <olemofo@gmail.com>
2026-02-26 22:50:22 +0100

    last changes

--- scenes/lobby/tscn/ContPlayers.cs
+++ scenes/lobby/tscn/ContPlayers.cs
@@ -1,19 +1,33 @@
 using Godot;
 using System;
-
-/*
-	Display the state of players within the 
-*/
+using System.Collections.Generic;
 
 public partial class ContPlayers : HFlowContainer
 {
-	// Called when the node enters the scene tree for the first time.
+	private readonly List<StatPlayerLobby> playerList = new();
+
 	public override void _Ready()
 	{
+		UpdateVisibility();
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	public void UpdateVisibility()
 	{
+		var children = GetChildren();
+
+		for (int i = 0; i < children.Count; i++)
+		{
+			if (children[i] is not Control slot)
+				continue;
+
+			// Show slot only if we have player data for it
+			slot.Visible = i < playerList.Count;
+		}
+	}
+
+	public struct StatPlayerLobby
+	{
+		public bool IsReady;
+		public string Name;
 	}
 }
--- scenes/lobby/tscn/CtrlPlayer.cs
+++ scenes/lobby/tscn/CtrlPlayer.cs
@@ -1,19 +1,9 @@
 using Godot;
 using System;
 
-public partial class CtrlPlayer : Control
-{
-	public TextureRect ImgReady;
-	
+public partial class CtrlPlayer : Control {
 	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		ImgReady = GetNode<TextureRect>("Img-Ready");
-		ImgReady.Visible = true;
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
+	public override void _Ready() {
+		
 	}
 }
